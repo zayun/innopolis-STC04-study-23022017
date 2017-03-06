b8c4f1bdb05078401d7886798a9f08c72f2b8be4
Egor Smoldyrev <esmoldyrev@gmail.com>
2017-03-06 21:06:43 +0300

    Spring MVC
    all controllers added

---
+++ src/main/java/com/smoldyrev/controllers/EditLectionController.java
@@ -0,0 +1,13 @@
+package com.smoldyrev.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by smoldyrev on 06.03.17.
+ */
+@Controller
+public class AddLectionController {
+
+
+}
---
+++ src/main/java/com/smoldyrev/controllers/EditStudentController.java
@@ -0,0 +1,7 @@
+package com.smoldyrev.controllers;
+
+/**
+ * Created by smoldyrev on 06.03.17.
+ */
+public class EditStudentController {
+}
